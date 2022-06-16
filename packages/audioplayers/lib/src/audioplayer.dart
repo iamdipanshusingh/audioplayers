0609af3fc16685ae28f983844caf25bbdcfda647
Dipanshu Singh <dipanshu.singh1997@gmail.com>
2022-06-16 17:44:31 +0530

    made some changes for urordr

--- packages/audioplayers/lib/src/audioplayer.dart
+++ packages/audioplayers/lib/src/audioplayer.dart
@@ -24,7 +24,7 @@ class AudioPlayer {
       const MethodChannel('xyz.luan/audioplayers')
         ..setMethodCallHandler(platformCallHandler);
 
-  static const _uuid = Uuid();
+  static final _uuid = Uuid();
 
   final StreamController<PlayerState> _playerStateController =
       StreamController<PlayerState>.broadcast();
